1c0373d31d89698fe4e23f0b738be24befb5f758
Samson <sfagade@gmail.com>
2020-10-07 19:21:11 +0100

    Created database connection with SQLAlchemy Changed form action url to use flask route

--- flask_and_python/app.py
+++ flask_and_python/app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/height_collector'
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email_ = email
+        self.height_ = height
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+        print(email, height)
+    return render_template("success.html")
 
 
 if __name__ == '__main__':

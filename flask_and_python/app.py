f6112d90cc463b8aa506118a8ebf39b3df155146
Samson <sfagade@gmail.com>
2020-10-09 19:22:34 +0100

    Added functionality to calculate height average

--- flask_and_python/app.py
+++ flask_and_python/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/height_collector'
@@ -28,11 +29,15 @@ def success():
     if request.method == 'POST':
         email = request.form["email_name"]
         height = request.form["height_name"]
-        send_email(email, height)
         if db.session.query(Data).filter(Data.email_ == email).count() < 1:
             data = Data(email, height)
             db.session.add(data)
             db.session.commit()
+            average_height = db.session.query(func.avg(Data.height_)).scalar()
+            average_height = round(average_height, 2)
+            count = db.session.query(Data.height_).count()
+            send_email(email, height, average_height, count)
+            print(average_height)
             return render_template("success.html")
 
         return render_template("index.html", text="Email address already exist")
--- flask_and_python/send_email.py
+++ flask_and_python/send_email.py
@@ -2,13 +2,14 @@ from email.mime.text import MIMEText
 import smtplib
 
 
-def send_email(email_address, height):
+def send_email(email_address, height, average_height, count):
     from_email = "saintwan@yahoo.com"
     from_password = "my_password"
     to_email = email_address
 
     subject = "Height Data"
-    message = "Hey there, your height is <b>%s</b>. " % height
+    message = "Hey there, your height is <b>%s</b>. Average Height of all is %s and that is calculated out of %s " \
+              " people." % (height, average_height, count)
 
     msg = MIMEText(message, "html")
     msg['Subject'] = subject

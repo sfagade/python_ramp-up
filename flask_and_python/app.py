0eaa41a86353b48ad3fa482b387e819977c27435
Samson <sfagade@gmail.com>
2020-10-09 21:50:40 +0100

    Added upload and download functionality to web app

--- flask_and_python/app.py
+++ flask_and_python/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
 from send_email import send_email
 from sqlalchemy.sql import func
 
@@ -24,6 +26,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/fileUpload")
+def file_upload():
+    return render_template("file_uploader.html")
+
+
 @app.route("/success", methods=['POST'])
 def success():
     if request.method == 'POST':
@@ -41,6 +48,25 @@ def success():
             return render_template("success.html")
 
         return render_template("index.html", text="Email address already exist")
+
+
+@app.route("/upload", methods=['POST'])
+def upload_file():
+    global file
+    if request.method == 'POST':
+        file = request.files["file_"]
+        file.save(secure_filename("uploaded"+file.filename))
+        with open("uploaded"+file.filename, "a") as f:
+            f.write("This was added now!")
+
+        print(type(file))
+        return render_template("file_uploader.html", btn="download.html")
+
+
+@app.route("/download")
+def download():
+    return send_file("uploaded" + file.filename, attachment_filename="yourfile.csv", as_attachment=True)
+
 
 
 if __name__ == '__main__':
